03f154001b8c4e85d401a457cfb18e48f2f9cc99
Hong Nguyen <nghongbk@gmail.com>
2015-08-31 16:15:44 +0700

    add checking length field for adding new post

--- assets/js/others/newPost.js
+++ assets/js/others/newPost.js
@@ -16,9 +16,38 @@ $('#new-post-form').validator().on('submit', function (e) {
       dataType: 'json',
       encode: true
     }).done(function (data) {
-      toastr.info('Post has been created successful!');
+      window.location.href = '/overview';
     }).fail(function (error) {
       toastr.error(error.responseText);
     });
   }
+});
+
+function checkLength(element, maxLength) {
+  var $element = $(element),
+      $helpBlock = $(element + ' ~ .help-block');
+
+  if ($element.val().length >= maxLength*0.9) {
+    $helpBlock.text('You should keep the text below ' + maxLength + 
+                    ' characters. Yours is: ' + $element.val().length);
+  }
+}
+
+$(document).ready(function () {
+  var $inputTitle           = $('#inputTitle'),
+      $inputMetaTitle       = $('#inputMetaTitle'),
+      $inputMetaDescription = $('#inputMetaDescription'),
+      $inputFacebookStatus  = $('#inputFacebookStatus');
+  $inputTitle.on('keydown keyup', function () {
+    $inputMetaTitle.attr('placeholder', $inputTitle.val());
+  });
+  $inputMetaTitle.on('keydown keyup', function () {
+    checkLength('#inputMetaTitle', 80);
+  });
+  $inputMetaDescription.on('keydown keyup', function () {
+    checkLength('#inputMetaDescription', 156);
+  });
+  $inputFacebookStatus.on('keydown keyup', function () {
+    checkLength('#inputFacebookStatus', 400);
+  });
 });
